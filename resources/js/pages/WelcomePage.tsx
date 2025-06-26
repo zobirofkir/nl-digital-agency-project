0df18eae8730c2fd979aae11968a3a1a15305914
zobirofkir <zobirofkir19@gmail.com>
2025-06-26 13:08:41 +0200

    add head

--- resources/js/pages/WelcomePage.tsx
+++ resources/js/pages/WelcomePage.tsx
@@ -1,9 +1,11 @@
 import AppLayout from '@/layouts/app-layout'
+import { Head } from '@inertiajs/react'
 import React from 'react'
 
 const WelcomePage = () => {
   return (
     <AppLayout>
+      <Head title="Accueil" />
       <div>
         Welcome Page
       </div>
