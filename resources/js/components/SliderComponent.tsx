6f84e6e1b52607cd64b24f9c1e4681a83d96cb4d
zobirofkir <zobirofkir19@gmail.com>
2025-06-26 14:20:09 +0200

    create slider component

--- resources/js/components/SliderComponent.tsx
+++ resources/js/components/SliderComponent.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import SliderBackground from '@/assets/slider/slider-background.png';
 
 const SliderComponent = () => {
   return (
-    <div>SliderComponent</div>
+    <div className="relative w-full min-h-screen h-screen">
+      {/* Background slider image */}
+      <div
+        className="absolute inset-0 w-full h-full z-0"
+        style={{
+          backgroundImage: `url(${SliderBackground})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 0.25, 
+          pointerEvents: 'none',
+        }}
+        aria-hidden="true"
+      />
+      {/* Slider content */}
+      <div className="relative z-10">
+        Slider Component
+      </div>
+    </div>
   )
 }
 
