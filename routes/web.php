e854f582292cf84c867c907596210c3c6a142229
zobirofkir <zobirofkir19@gmail.com>
2025-06-26 12:55:09 +0200

    clean code

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return inertia('WelcomePage');
 })->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
