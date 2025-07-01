c5517fe2d48a2d7d713614111c045c4222657412
zobirofkir <zobirofkir19@gmail.com>
2025-07-01 12:35:01 +0200

    update

--- resources/js/pages/WelcomePage.tsx
+++ resources/js/pages/WelcomePage.tsx
@@ -1,23 +1,22 @@
 import SliderComponent from '@/components/SliderComponent'
 import AppLayout from '@/layouts/app-layout'
 import { Head } from '@inertiajs/react'
-import React from 'react'
-import HeaderComponent from '@/components/HeaderComponent'
 import AboutComponent from '@/components/about/AboutComponent'
-import FlushImage from '@/assets/abouts/about-flush.png'
 
 const WelcomePage = () => {
   return (
     <AppLayout>
       <Head title="Accueil" />      
-      <section>
-          <SliderComponent />        
-      </section>
-      <section>
+      <section className='overflow-hidden'>
 
-      </section>
-      <section>
-        <AboutComponent />
+        <div>
+            <SliderComponent />        
+        </div>
+
+        <div>
+          <AboutComponent />
+        </div>
+
       </section>
     </AppLayout>
   )
