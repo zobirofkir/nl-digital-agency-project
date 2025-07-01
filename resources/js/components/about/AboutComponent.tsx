3dd6987ad0ae6a3b622a60c3fbaf5e4d4556b557
zobirofkir <zobirofkir19@gmail.com>
2025-07-01 14:08:09 +0200

    update

--- resources/js/components/about/AboutComponent.tsx
+++ resources/js/components/about/AboutComponent.tsx
@@ -200,13 +200,13 @@ const AboutComponent = () => {
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                   </svg>
-                  contact@nldigital.ma
+                  contact@nl-digitalagency.com
                 </div>
                 <div className="flex items-center gap-2">
                   <svg className="w-3 h-3 text-red-400" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
                   </svg>
-                  Casablanca, Maroc
+                  Fés, Maroc
                 </div>
               </div>
             </motion.div>
