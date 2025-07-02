959db0e58474f8807cb382ac87233f419ad01655
zobirofkir <zobirofkir19@gmail.com>
2025-07-02 12:48:58 +0200

    create service page

---
+++ resources/js/pages/ServicePage.tsx
@@ -0,0 +1,14 @@
+import AppLayout from '@/layouts/app-layout'
+import React from 'react'
+
+const ServicePage = () => {
+  return (
+    <AppLayout>
+      <div>
+        
+      </div>
+    </AppLayout>
+  )
+}
+
+export default ServicePage
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return inertia('WelcomePage');
 })->name('home');
+
+Route::get('/services', function () {
+    return inertia('ServicePage');
+})->name('services');
