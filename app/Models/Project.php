100d4c9d33c81fa6cc94b6f6ce351120c59a25f4
zobirofkir <zobirofkir19@gmail.com>
2025-07-03 16:30:46 +0200

    create project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "title",
+        "image",
+        "description"
+    ];
 }
