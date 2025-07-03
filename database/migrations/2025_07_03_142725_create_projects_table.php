05d577fc77bea1f01a747e62608d019a9f2944c3
zobirofkir <zobirofkir19@gmail.com>
2025-07-03 16:29:52 +0200

    create project migration

--- database/migrations/2025_07_03_142725_create_projects_table.php
+++ database/migrations/2025_07_03_142725_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('title');
+            $table->longText('image');
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
