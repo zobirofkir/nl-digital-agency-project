db784fd33251da9ca8f05e15baf368bf5974c4f6
zobirofkir <zobirofkir19@gmail.com>
2025-07-07 12:58:09 +0200

    create contact store

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
+use App\Http\Resources\ContactResource;
 
 class ContactController extends Controller
 {
     public function index()
     {
         return inertia('ContactPage');
+    }
+
+    public function store(ContactRequest $request)
+    {
+        return ContactResource::make(
+            Contact::create($request->validated())
+        );
     }
 }
